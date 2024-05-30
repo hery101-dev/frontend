5048a36c58a6a77ef52c83659e9a96708c8777e4
hery101-dev <141057845+hery101-dev@users.noreply.github.com>
2024-05-30 15:08:59 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-	 publicPath: process.env.NODE_ENV === 'production' ? '/subfolder/' : '/',
+	 publicPath: process.env.NODE_ENV === '/https://github.com/hery101-dev/frontend/',
   devServer: {
     proxy: 'http://localhost:8000'
 },
